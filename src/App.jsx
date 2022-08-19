4b8d871ba910a849781fa0e0903b5cbd6fd5b72f
Vasile Midrigan <111rigan111@gmail.com>
2022-08-19 22:30:34 +0100

    Specificity

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { BookList } from "./pages/BookList";
 import { Book } from "./pages/Book";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
   return (
@@ -20,7 +22,19 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books" element={<BookList />} />
+        {/* Specificity: 
+               When we have a hardcoded Route and
+               dynamic one, React will open the 
+               hardcoded Route. We can say hardcoded 
+               route is more specific.
+        
+        */}
         <Route path="/books/:id" element={<Book />} />
+        <Route path="/books/new" element={<NewBook />} />
+        {/*  
+            404 kind of page:
+          */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
